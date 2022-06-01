e9a554ae4ef7b5d17f24de3bd13bbb4d314ceac3
Lily <yoyo.spot.blog@gmail.com>
2022-06-01 10:55:50 +0300

    writing in Page Object style

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,17 @@
 from selenium.webdriver.common.by import By
+from pages.main_page import MainPage #для windows убрать точку перед pages
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина, pytest -v --tb=line --language=en test_main_page.py
+
+"""
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link) #page.open()   
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link") #page.go_to_login_page()
     login_link.click() #pytest -v --tb=line --language=en test_main_page.py
+
+"""
