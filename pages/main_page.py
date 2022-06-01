a7867223dbf82fffb8f78f51858a8dff89002346
Lily <yoyo.spot.blog@gmail.com>
2022-06-01 13:22:02 +0300

    locators in another file

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
 from .base_page import BasePage #если модуль из той же папки указывается точка
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK) #символ * указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
